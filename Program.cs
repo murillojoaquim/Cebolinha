e408f87980e5d4b455d1fa821a7f84889834e3c6
murillo <murillojalves@hotmail.com>
2021-09-01 18:50:35 -0300

    Fala cebolinha

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write("Digite uma frase para o cebolinha dizer: ");
+            string texto = Console.ReadLine();
+            
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("\nCebolinha: ");
+            
+            Console.ResetColor();
+            Console.WriteLine(texto.Replace("r", "l"));
+
+            Console.ResetColor();
+            
+
+        }
+    }
+}
